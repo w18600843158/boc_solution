573981f413d4bd780368d54fa7a0a27a228a8f9c
w18600843158 <260023590@qq.com>
2022-03-12 16:00:41 +0800

    要在 boc 承担开发工作了, 为了更好地学习 git, 以及提高开发技巧, 要开始刷题之旅!

---
+++ src/main/java/com/wxw/boc_solution/BocSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.wxw.boc_solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BocSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BocSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wxw/boc_solution/BocSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxw.boc_solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BocSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
